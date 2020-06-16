48fa1161101036d8c8defab81e9ee9b2030af249
MicheleK17 <michele.kayser@bfr.bund.de>
2020-06-16 11:34:04 +0200

    Determine Styling strategy
    	Styling implementation with Emotion library
    
    Ticket: #7
    Change-Id: Id5f5b17f593579862e917153d1a11aa59f16a212

---
+++ src/app/components/Style/Style-MainTheme.component.tsx
@@ -0,0 +1,75 @@
+
+/*
+BfR Colours
+*/
+
+export const bfrBlue = 'rgb(3, 78, 162)'; // #034ea2
+export const bfrWhite = 'rgb(255, 255, 255)'; // #ffffff
+export const bfrLightgrey = 'rgb(204, 204, 204)'; // #cccccc
+export const bfrBlack = 'rgb(0, 0, 0)'; // #000000
+export const bfrLightblue = 'rgb(140, 190, 218)'; // #8cbeda
+export const bfrLightcyan = 'rgb(209, 237, 244)'; // #d1edf4
+export const bfrOcre = 'rgb(241, 199, 83)'; // #f1c753
+export const bfrLightocre = 'rgb(246, 231, 187)'; // #f6e7bb
+export const bfrDarkgrey = 'rgb(102, 102, 102)'; // #666666
+export const bfrMintgreen = 'rgb(52, 172, 159)'; // #34ac9f
+export const bfrRed = 'rgb(254, 0, 0)'; // #fe0000
+
+export const primaryColor = bfrBlue;
+export const secondaryColor = bfrOcre;
+export const backgroundColor = bfrWhite;
+export const surfaceColor = bfrLightgrey;
+export const errorColor = bfrRed;
+export const onPrimaryColor = bfrWhite;
+export const onSecondaryColor = bfrBlack;
+export const onBackgroundColor = bfrBlack;
+export const onSurfaceColor = bfrBlack;
+export const onErrorColor = bfrBlack;
+
+/*
+* Theme generated using this tool: http://mcg.mbitson.com
+* The primary color (value 500) used was the bfr-blue.
+*/
+
+export const bfrPrimaryPalette = {
+    50: '#e1eaf4',
+    100: '#b3cae3',
+    200: '#81a7d1',
+    300: '#4f83be',
+    400: '#2969b0',
+    500: primaryColor,
+    600: '#03479a',
+    700: '#023d90',
+    800: '#023586',
+    900: '#012575',
+    A100: '#a3b8ff',
+    A200: '#7090ff',
+    A400: '#3d69ff',
+    A700: '#2455ff',
+    contrast: {
+        50: bfrBlack,
+        100: bfrBlack,
+        200: bfrBlack,
+        300: onPrimaryColor,
+        400: onPrimaryColor,
+        500: onPrimaryColor,
+        600: onPrimaryColor,
+        700: onPrimaryColor,
+        800: onPrimaryColor,
+        900: onPrimaryColor,
+        A100: bfrBlack,
+        A200: bfrBlack,
+        A400: onPrimaryColor,
+        A700: onPrimaryColor,
+    }
+};
+
+/*
+* Shadow coloring 
+*/
+
+export const shadowPalette = {
+    shadow1: 'rgba(0, 0, 0, 0.2)',
+    shadow2: 'rgba(0, 0, 0, 0.14)',
+    shadow3: 'rgba(0, 0, 0, 0.12)',
+}
