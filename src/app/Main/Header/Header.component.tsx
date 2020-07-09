e3f64decb530d427415eb3136ecd9468a9f0c25e
MicheleK17 <michele.kayser@bfr.bund.de>
2020-07-09 13:01:07 +0200

    Change translation-buttons to flags
    	Using the German and the British flag to change the language
    
    Ticket: #21
    Change-Id: Icccbbe8e6087ec5ef5814f0e33fe65a604b300c9

--- src/app/Main/Header/Header.component.tsx
+++ src/app/Main/Header/Header.component.tsx
@@ -8,26 +8,36 @@ import { primaryColor, onPrimaryColor } from '../../Shared/Style/Style-MainTheme
 const headerStyle = css`
     width: 100%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;  
+`
+const mainHeaderStyle = css`
+    padding: 0.5em 0 0.5em 1em;
+    box-shadow: 0 2px 6px 0 grey;
     background-color: ${primaryColor};
 `
 const appNameStyle = css`
-    padding: 0.75em;
-    font-size: 2rem;
-    font-weight: bold;
-    color: ${onPrimaryColor};
+    padding: 0.5em 0 0.5em 1em;
+    font-size: 1.2rem;
     text-decoration: none;
+    color: ${onPrimaryColor};
 `
-
+const translationHeaderStyle = css`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+`
 
 export function HeaderLayoutComponent(): JSX.Element {
     return (
         <header css={headerStyle}>
-            <NavLink to="/" css={appNameStyle}>
-                ZooNotify
-            </NavLink>
-            <TranslationButtons />
+            <div css={mainHeaderStyle}>
+                <NavLink to="/" css={appNameStyle}>
+                    ZooNotify
+                </NavLink>
+            </div>
+            <div css={translationHeaderStyle}>
+                <TranslationButtons />
+            </div>
         </header>
     );
 }
--- src/app/Main/Header/TranslationButtons.component.tsx
+++ src/app/Main/Header/TranslationButtons.component.tsx
@@ -1,22 +1,33 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { useTranslation } from 'react-i18next';
-import { Button } from '@material-ui/core';
 import { changeAppLanguage } from '../../Core/localization.service';
+import { backgroundColor } from '../../Shared/Style/Style-MainTheme.component';
 
 
 const buttonStyle = css`
-    padding: 1em;
+    padding: 0.5em;  
+`
+const flagStyle = css`
+    margin: 0.5em;
+    padding: 0;
+    cursor: pointer;
+    background-color: ${backgroundColor};
+    border: none;
 `
 
+function countryToFlag(isoCode: string): string {
+    return typeof String.fromCodePoint !== 'undefined'
+      ? isoCode
+          .toUpperCase()
+          .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
+      : isoCode;
+}
 
 export function TranslationButtonsComponent(): JSX.Element {
-    const { t } = useTranslation(['Header']);
     return (
         <div css={buttonStyle}>
-            <Button variant="contained" color="primary" onClick={()=>changeAppLanguage('de')} >{t('Button.de')}</Button>
-            <Button variant="contained" color="primary" onClick={()=>changeAppLanguage('en')}>{t('Button.en')}</Button>
+            <button type='button' onClick={()=>changeAppLanguage('de')} css={flagStyle}>{countryToFlag('DE')}</button>
+            <button type='button' onClick={()=>changeAppLanguage('en')} css={flagStyle}>{countryToFlag('GB')}</button>
         </div>
     );
 }
-
