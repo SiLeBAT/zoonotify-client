2535f3acdafae454d891659e281c8080277a7b94
MicheleK17 <michele.kayser@bfr.bund.de>
2020-07-23 10:27:09 +0200

    As a User I would like to export the currently displayed table data into a CSV file
    	Add button to download the currently displayed table as CSV
    
    Ticket: #31
    Change-Id: Iafe916f1260bf343a7929864b72790d490185443

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import React, { Suspense } from 'react';
 import * as ReactDOM from 'react-dom';
+import { StylesProvider } from '@material-ui/core/styles';
 import { MainLayoutComponent } from "./Main/Main-Layout.component";
 import '../i18n';
 
 
+
+
 ReactDOM.render(
     <Suspense fallback={(<div>Loading</div>)}>
-        <MainLayoutComponent />
+        <StylesProvider injectFirst>
+            <MainLayoutComponent />
+        </StylesProvider>       
     </Suspense>,
     document.querySelector("#application")
 );
