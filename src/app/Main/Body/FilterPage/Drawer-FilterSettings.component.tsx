b6fb41fd0eee8c54c3e0d7bca228a5ac4c24ce6a
MicheleK17 <michele.kayser@bfr.bund.de>
2020-08-17 16:23:07 +0200

    As a User I would like to filter the currently displayed table data by "Serovar"
        Using React Context to handle loading data and filter setting
    
    Ticket: #51
    Change-Id: I61a4633379a2f43ce1ae4267e22fa5560ecbfe2a

--- src/app/Main/Body/FilterPage/Drawer-FilterSettings.component.tsx
+++ src/app/Main/Body/FilterPage/Drawer-FilterSettings.component.tsx
@@ -1,13 +1,16 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import React from "react";
+import React, { useContext } from "react";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import IconButton from "@material-ui/core/IconButton";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 import { useTranslation } from "react-i18next";
+import { FilterContext } from "../../../Shared/Context/FilterContext";
+import { DataContext } from "../../../Shared/Context/DataContext";
 import { primaryColor } from "../../../Shared/Style/Style-MainTheme.component";
+import { SelectorItem } from "./Drawer-SelectorItem.component";
 
 const filterHeadingStyle = css`
     margin-top: 0.5em;
@@ -28,7 +31,7 @@ const filterSubheadingStyle = css`
 
 const selectorAreaStyle = css`
     width: -webkit-fill-available;
-    margin: 2em;
+    margin: 0 2em 2em 2em;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -49,30 +52,33 @@ const selectorStyle = css`
         padding: 0.8em;
     }
 `;
+const selectorHeadingStyle = css`
+    padding: 0;
+    margin: 2em 2em 0 2em;
+`;
 
 export function FilterSettingsComponent(): JSX.Element {
     const [state, setState] = React.useState<{
         number: number;
-        rows: JSX.Element[];
-        pathogen: string;
-        serovar: string;
+        filter: string;
     }>({
-        rows: [],
         number: 1,
-        pathogen: "",
-        serovar: "",
+        filter: "none",
     });
+    const { setFilter } = useContext(FilterContext);
+    const dataValue = useContext(DataContext);
 
     const { t } = useTranslation(["FilterPage"]);
 
     const handleChange = (
-        event: React.ChangeEvent<{ name?: string; value: unknown }>
+        event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>
     ): void => {
-        const name = event.target.name as keyof typeof state;
+        const name = event.target.value as keyof typeof state;
         setState({
             ...state,
-            [name]: event.target.value,
+            filter: name,
         });
+        setFilter({ filterValue: name });
     };
 
     const handleAdd = (): void => {
@@ -88,6 +94,12 @@ export function FilterSettingsComponent(): JSX.Element {
         });
     };
 
+    const filterValues = dataValue.data.uniqueValues;
+
+    const selectorItems = filterValues.map((item: string) => (
+        <SelectorItem item={item} />
+    ));
+
     return (
         <div>
             <h3 css={filterHeadingStyle}>{t("Drawer.Title")}</h3>
@@ -102,37 +114,35 @@ export function FilterSettingsComponent(): JSX.Element {
                     const elements = [];
                     for (let i = 0; i < state.number; i += 1) {
                         elements.push(
-                            <div css={selectorAreaStyle} key={i}>
-                                <FormControl
-                                    variant="filled"
-                                    css={selectorStyle}
-                                >
-                                    <Select
-                                        native
-                                        value={state.pathogen}
-                                        onChange={handleChange}
-                                        inputProps={{
-                                            name: "pathogen",
-                                            id: "filled-age-native-simple",
-                                        }}
+                            <div>
+                                <h5 css={selectorHeadingStyle}>Serovar</h5>
+                                <div css={selectorAreaStyle} key={i}>
+                                    <FormControl
+                                        variant="filled"
+                                        css={selectorStyle}
                                     >
-                                        <option value="">
-                                            {t("Drawer.Selector")}
-                                        </option>
-                                        <option value="pathogen">
-                                            {t("Drawer.Filters.Pathogen")}
-                                        </option>
-                                        <option value="serovar">
-                                            {t("Drawer.Filters.Serovar")}
-                                        </option>
-                                    </Select>
-                                </FormControl>
-                                <IconButton
-                                    css={iconButtonStyle}
-                                    onClick={handleRemove}
-                                >
-                                    <RemoveCircleIcon css={iconStyle} />
-                                </IconButton>
+                                        <Select
+                                            native
+                                            value={state.filter}
+                                            onChange={handleChange}
+                                            inputProps={{
+                                                name: "filter",
+                                                id: "filled-age-native-simple",
+                                            }}
+                                        >
+                                            <option value="">
+                                                {t("Drawer.Selector")}
+                                            </option>
+                                            {selectorItems}
+                                        </Select>
+                                    </FormControl>
+                                    <IconButton
+                                        css={iconButtonStyle}
+                                        onClick={handleRemove}
+                                    >
+                                        <RemoveCircleIcon css={iconStyle} />
+                                    </IconButton>
+                                </div>
                             </div>
                         );
                     }
