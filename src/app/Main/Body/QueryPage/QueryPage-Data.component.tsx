b68c44ae5e17bc7031aba76b3c41a8de199cf017
MicheleK17 <michele.kayser@bfr.bund.de>
2020-08-21 09:33:42 +0200

    Implement MainFilter "Erreger"
    Ticket: #61
    Change-Id: I754ce3f3529e4ff5b005d824ab99340bf4662c72

--- src/app/Main/Body/QueryPage/QueryPage-Data.component.tsx
+++ src/app/Main/Body/QueryPage/QueryPage-Data.component.tsx
@@ -1,12 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { useContext } from "react";
-import {
-    withStyles,
-    /* Theme, */
-    createStyles,
-    makeStyles,
-} from "@material-ui/core/styles";
+import { useContext, useState, useEffect } from "react";
+import { withStyles, createStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -16,10 +11,14 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import _ from "lodash";
 import { useTranslation } from "react-i18next";
-import { primaryColor, onBackgroundColor } from "../../../Shared/Style/Style-MainTheme.component";
+import {
+    primaryColor,
+    onBackgroundColor,
+} from "../../../Shared/Style/Style-MainTheme.component";
 import { FilterContext } from "../../../Shared/Context/FilterContext";
 import { DataContext } from "../../../Shared/Context/DataContext";
-
+import { DBentry } from "../../../Shared/Isolat.model";
+import { FilterType } from "../../../Shared/Filter.model";
 
 const dataStyle = css`
     box-sizing: inherit;
@@ -36,7 +35,7 @@ const useStyles = makeStyles({
         wordWrap: "break-word",
         textAlign: "center",
         padding: "0.75em",
-        color: onBackgroundColor
+        color: onBackgroundColor,
     },
 });
 
@@ -51,22 +50,57 @@ const StyledTableCell = withStyles(() =>
             color: onBackgroundColor,
             fontSize: 14,
         },
-    }))(TableCell);
+    })
+)(TableCell);
+
 
 export function QueryPageTableComponent(): JSX.Element {
+    const [isolates, setIsolates] = useState<number>();
     const classes = useStyles();
     const { filter } = useContext(FilterContext);
-    const { data } = useContext(DataContext);
+    const { data, setData } = useContext(DataContext);
     const { t } = useTranslation(["QueryPage"]);
-    let filterData = [];
-
-    if (filter.filterValue !== "") {
-        filterData = _.filter(data.ZNData, { Serovar: filter.filterValue });
-    } else {
+    let filterData: DBentry[] = [];
+    
+    const useFilter = async (): Promise<void> => {
         filterData = data.ZNData;
-    }
+        const filterKeyValues = Object.keys(filter) as FilterType[];
+        await Promise.all(
+            filterKeyValues.map(async (attribute) => {
+                const filterValues = filter[attribute];
+                let newFilterData: DBentry[] = [];
+                await Promise.all(
+                    filterValues.map(async (value) => {
+                        const partialFilterData = _.filter(filterData, {
+                            [attribute]: value,
+                        });
+                        newFilterData = newFilterData.concat(partialFilterData);
+                    })
+                );
+                filterData = newFilterData;
+            })
+        );
+        setData({...data, ZNDataFiltered: filterData})
+        setIsolates(Object.keys(filterData).length);
+    };
 
-    const isolates = Object.keys(filterData).length;
+    const noFilter = Object.keys(filter).every(function emptyArray(
+        key
+    ): boolean {
+        const k = key as FilterType;
+        const empty = _.isEmpty(filter[k]);
+        return empty;
+    });
+
+    useEffect((): void => {
+        if (noFilter) {
+            filterData = data.ZNData;
+            setData({...data, ZNDataFiltered: data.ZNData})
+            setIsolates(Object.keys(filterData).length);
+        } else {
+            useFilter();
+        }
+    }, [filter]);
 
     return (
         <div css={dataStyle}>
@@ -74,7 +108,9 @@ export function QueryPageTableComponent(): JSX.Element {
                 <Table stickyHeader aria-label="simple table">
                     <TableHead>
                         <TableRow key="headerRow">
-                            <StyledTableCell>{t("Results.TableHead")}</StyledTableCell>
+                            <StyledTableCell>
+                                {t("Results.TableHead")}
+                            </StyledTableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
