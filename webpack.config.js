cd2a0b36a42ac8414e8dc04468397ad845072760
Dominic Tölle <dominic.tolle@gmail.com>
2020-09-09 10:15:53 +0200

    Fix Routing of the dev server
    Change-Id: Ic963a5855e95b8feae3fbf3fff43d5db7c034a0d
    Ticket: #72

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,46 @@
 const path = require("path");
 module.exports = {
-  mode: "development",
-  entry: "./src/app/App.tsx",
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".json", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: "awesome-typescript-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
+    mode: "development",
+    entry: "./src/app/App.tsx",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: {
+          index: 'index.html'
+        },
+        proxy: {
+            "/v1": {
+                target: "http://localhost:3000",
+            },
+            "/api-docs": {
+                target: "http://localhost:3000",
+            }
+        },
+    },
+    resolve: {
+        extensions: [".js", ".json", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "awesome-typescript-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
 };
