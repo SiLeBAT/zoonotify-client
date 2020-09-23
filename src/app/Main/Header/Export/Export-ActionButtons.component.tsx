47d3a7df728a797f131eaa0fd94d7acb683090b0
MicheleK17 <michele.kayser@bfr.bund.de>
2020-09-23 17:17:54 +0200

    Implement warning, if no checkbox is selected
        - disable export, if no checkbox is selected
    
    Ticket: #81
    Change-Id: I7b69203047172cb0c95cca3649a4f65256bbb251

--- src/app/Main/Header/Export/Export-ActionButtons.component.tsx
+++ src/app/Main/Header/Export/Export-ActionButtons.component.tsx
@@ -1,20 +1,24 @@
 /** @jsx jsx */
-import { css, jsx } from "@emotion/core";
+import { css, jsx, SerializedStyles } from "@emotion/core";
 import DownloadLink from "react-download-link";
 import { Button, DialogActions } from "@material-ui/core";
-import { primaryColor } from "../../../Shared/Style/Style-MainTheme.component";
 import { objectToCsv } from "../../../Core/DBEntriesToCSV.service";
 import {
     ExportInterface,
     MainFilterLabelInterface,
 } from "../../../Shared/Export.model";
 import { FilterInterface } from "../../../Shared/Filter.model";
+import { errorColor } from "../../../Shared/Style/Style-MainTheme.component";
 
 const ButtonLinkStyle = css`
-    margin: 0px !important;
+    all: inherit !important;
+`;
+
+const warningStyle = (isSelect: boolean): SerializedStyles => css`
+    display: ${isSelect ? "none" : "flex"};
+    color: ${errorColor};
+    margin-left: 2em;
     font-size: 0.75rem;
-    text-decoration: none !important;
-    color: ${primaryColor} !important;
 `;
 
 interface ExportActionButtonProps {
@@ -35,26 +39,36 @@ export function ExportActionButtonComponent(
         props.onClick(event);
     };
 
+    let fileIsSelect = true;
+    if (!props.setting.raw && !props.setting.stat) {
+        fileIsSelect = false;
+    }
+
     return (
-        <DialogActions>
-            <Button onClick={handleClose} color="primary" css={ButtonLinkStyle}>
-                Cancel
-            </Button>
-            <Button onClick={handleClose} color="primary">
-                <DownloadLink
-                    label={props.buttonLabel}
-                    filename={props.ZNFilename}
-                    exportFile={() =>
-                        objectToCsv({
-                            setting: props.setting,
-                            filter: props.filter,
-                            allFilterLabel: props.allFilterLabel,
-                            mainFilterLabels: props.mainFilterLabels,
-                        })
-                    }
-                    css={ButtonLinkStyle}
-                />
-            </Button>
-        </DialogActions>
+        <div>
+            <p css={warningStyle(fileIsSelect)}>
+                Please select at least one option or cancel the export
+            </p>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+                </Button>
+                <Button color="primary" disabled={!fileIsSelect}>
+                    <DownloadLink
+                        label={props.buttonLabel}
+                        filename={props.ZNFilename}
+                        exportFile={() =>
+                            objectToCsv({
+                                setting: props.setting,
+                                filter: props.filter,
+                                allFilterLabel: props.allFilterLabel,
+                                mainFilterLabels: props.mainFilterLabels,
+                            })
+                        }
+                        css={ButtonLinkStyle}
+                    />
+                </Button>
+            </DialogActions>
+        </div>
     );
 }
