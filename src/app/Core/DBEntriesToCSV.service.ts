70eaea0bebd3929ce1006b3955b5ba70f513e8e2
MicheleK17 <michele.kayser@bfr.bund.de>
2020-09-23 15:43:33 +0200

    Refactor DBEntriesToCSV service
        - replace the redundant code with a function.
    
    Ticket: #81
    Change-Id: Ia3d38f6a0c390ef39ee83621b50ec1279b13dc79

--- src/app/Core/DBEntriesToCSV.service.ts
+++ src/app/Core/DBEntriesToCSV.service.ts
@@ -1,28 +1,33 @@
 import _ from "lodash";
 import { DBentry, DBtype } from "../Shared/Isolat.model";
-import { FilterInterface, FilterType, mainFilterAttributes } from "../Shared/Filter.model";
+import { FilterInterface, mainFilterAttributes } from "../Shared/Filter.model";
+import { ExportInterface } from "../Shared/Export.model";
 
+function stringModification(inputString: string): string {
+    return inputString.replace(/"/g, '\\"').replace("undefined", "");
+}
+
+function generateCSVString<
+    T extends Record<string, string> | DBentry,
+    K extends keyof T
+>(dataArray: T[], headers: K[]): string {
+    const csvTable: string[] = [];
+
+    dataArray.forEach((row) => {
+        const values: string[] = headers.map((header) => {
+            return stringModification(`${row[header]}`);
+        });
+        csvTable.push(values.join(","));
+    });
+    const csvTableData: string = csvTable.join("\n");
+    return csvTableData;
+}
 
 interface ObjectToCsvProps {
-    setting: {
-        raw: boolean;
-        stat: boolean;
-        tableAttributes: {
-            row: FilterType,
-            column: FilterType,
-        };
-        rawDataSet: {
-            rawData: DBentry[];
-            rawKeys: DBtype[]; 
-        }; 
-        statDataSet: {
-            statData: Record<string, string>[];
-            statKeys:string[];
-        };
-    }
+    setting: ExportInterface;
     filter: FilterInterface;
     allFilterLabel: string;
-    mainFilterLabels: { Erreger: string; Matrix: string; Projektname: string};
+    mainFilterLabels: { Erreger: string; Matrix: string; Projektname: string };
 }
 
 export function objectToCsv(props: ObjectToCsvProps): string {
@@ -34,9 +39,15 @@ export function objectToCsv(props: ObjectToCsvProps): string {
 
     mainFilterAttributes.forEach((element): void => {
         if (props.filter[element].length !== 0) {
-            csvRows.push(`###${props.mainFilterLabels[element]}:"${props.filter[element].join('""')}"`);
+            csvRows.push(
+                `###${props.mainFilterLabels[element]}:"${props.filter[
+                    element
+                ].join('""')}"`
+            );
         } else {
-            csvRows.push(`###${props.mainFilterLabels[element]}:${props.allFilterLabel}`);
+            csvRows.push(
+                `###${props.mainFilterLabels[element]}:${props.allFilterLabel}`
+            );
         }
     });
     csvRows.push("####################");
@@ -44,49 +55,32 @@ export function objectToCsv(props: ObjectToCsvProps): string {
     if (props.setting.raw && !props.setting.stat) {
         const headers: DBtype[] = props.setting.rawDataSet.rawKeys;
         csvRows.push(headers.join(","));
-    
-        props.setting.rawDataSet.rawData.forEach((row: DBentry) => {
-            const values: string[] = headers.map((header: DBtype) => {
-                const escaped: string = `${row[header]}`
-                    .replace(/"/g, '\\"')
-                    .replace("undefined", "");
-                return `"${escaped}"`;
-            });
-            csvRows.push(values.join(","));
-        });
-        const csvData: string = csvRows.join("\n");
-        return csvData;
-    } 
-    
+        csvRows.push(
+            generateCSVString(props.setting.rawDataSet.rawData, headers)
+        );
+
+        return csvRows.join("\n");
+    }
+
     if (props.setting.stat && !props.setting.raw) {
-        if (!_.isEmpty(props.setting.tableAttributes.column)){
+        if (!_.isEmpty(props.setting.tableAttributes.column)) {
             csvRows.push(`,${props.setting.tableAttributes.column}`);
         }
         const headers: string[] = props.setting.statDataSet.statKeys;
-        if (!_.isEmpty(props.setting.tableAttributes.row)){
+        if (!_.isEmpty(props.setting.tableAttributes.row)) {
             const headerToPrint: string[] = [...headers];
-            headerToPrint[0] = props.setting.tableAttributes.row; 
+            headerToPrint[0] = props.setting.tableAttributes.row;
             csvRows.push(headerToPrint.join(","));
         } else {
             csvRows.push(headers.join(","));
         }
-       
-        props.setting.statDataSet.statData.forEach((row: Record<string, string>) => {
-            const values: string[] = headers.map((header: string) => {
-                const escaped: string = `${row[header]}`
-                    .replace(/"/g, '\\"')
-                    .replace("undefined", "");
-                return `"${escaped}"`;
-            });
-            csvRows.push(values.join(","));
-        });
-        const csvData: string = csvRows.join("\n");
-        return csvData;
+
+        csvRows.push(
+            generateCSVString(props.setting.statDataSet.statData, headers)
+        );
+
+        return csvRows.join("\n");
     }
-    
 
-    return "no data selected"
-   
-
-    
+    return "no data selected";
 }
