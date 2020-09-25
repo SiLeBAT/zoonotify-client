b7a19768d6e9d6847c9dc0903cf6fd722866612a
MicheleK17 <michele.kayser@bfr.bund.de>
2020-09-25 13:52:03 +0200

    Refactor export-services
        - devide DBEntriesToCSV.ts into multiple services
    
    Ticket: #81
    Change-Id: Ifa65f4fd55ed55435f47c82f53ee013ec6a43e6d

--- src/app/Core/DBEntriesToCSV.service.ts
+++ src/app/Core/DBEntriesToCSV.service.ts
@@ -1,27 +1,8 @@
-import _ from "lodash";
-import { DBentry, DBtype } from "../Shared/Isolat.model";
-import { FilterInterface, mainFilterAttributes } from "../Shared/Filter.model";
+import { FilterInterface } from "../Shared/Filter.model";
 import { ExportInterface } from "../Shared/Export.model";
-
-function stringModification(inputString: string): string {
-    return `"${inputString.replace(/"/g, '\\"').replace("undefined", "")}"`;
-}
-
-function generateCSVString<
-    T extends Record<string, string> | DBentry,
-    K extends keyof T
->(dataArray: T[], headers: K[]): string {
-    const csvTable: string[] = [];
-
-    dataArray.forEach((row) => {
-        const values: string[] = headers.map((header) => {
-            return stringModification(`${row[header]}`);
-        });
-        csvTable.push(values.join(","));
-    });
-    const csvTableData: string = csvTable.join("\n");
-    return csvTableData;
-}
+import { generateParameterHeader } from "./ExportServices/generateParameterHeader.service";
+import { RAWDataStringGenerator } from "./ExportServices/generateRAWString.service";
+import { statDataStringGenerator } from "./ExportServices/generateStatString.service";
 
 interface ObjectToCsvProps {
     setting: ExportInterface;
@@ -32,55 +13,26 @@ interface ObjectToCsvProps {
 
 export function objectToCsv(props: ObjectToCsvProps): string {
     const csvRows: string[] = [];
-
-    csvRows.push("\uFEFF");
-    csvRows.push("####################");
-    csvRows.push("#####Parameter:");
-
-    mainFilterAttributes.forEach((element): void => {
-        if (props.filter[element].length !== 0) {
-            csvRows.push(
-                `###${props.mainFilterLabels[element]}:"${props.filter[
-                    element
-                ].join('""')}"`
-            );
-        } else {
-            csvRows.push(
-                `###${props.mainFilterLabels[element]}:${props.allFilterLabel}`
-            );
-        }
-    });
-    csvRows.push("####################");
+    csvRows.push(
+        generateParameterHeader({
+            filter: props.filter,
+            allFilterLabel: props.allFilterLabel,
+            mainFilterLabels: props.mainFilterLabels,
+        })
+    );
 
     if (props.setting.raw && !props.setting.stat) {
-        const headers: DBtype[] = props.setting.rawDataSet.rawKeys;
-        csvRows.push(headers.join(","));
         csvRows.push(
-            generateCSVString(props.setting.rawDataSet.rawData, headers)
+            RAWDataStringGenerator(
+                props.setting.rawDataSet.rawKeys,
+                props.setting.rawDataSet.rawData
+            )
         );
-
-        return csvRows.join("\n");
     }
 
     if (props.setting.stat && !props.setting.raw) {
-        if (!_.isEmpty(props.setting.tableAttributes.column)) {
-            csvRows.push(`,${props.setting.tableAttributes.column}`);
-        }
-        const headers: string[] = props.setting.statDataSet.statKeys;
-        if (!_.isEmpty(props.setting.tableAttributes.row)) {
-            const headerToPrint: string[] = [...headers];
-            headerToPrint[0] = props.setting.tableAttributes.row;
-            csvRows.push(headerToPrint.join(","));
-        } else {
-            csvRows.push(headers.join(","));
-        }
-
-        csvRows.push(
-            generateCSVString(props.setting.statDataSet.statData, headers)
-        );
-
-        return csvRows.join("\n");
+        csvRows.push(statDataStringGenerator(props.setting));
     }
 
-    return "no data selected";
+    return csvRows.join("\n");
 }
---
+++ src/app/Core/ExportServices/generateRAWString.service.ts
@@ -0,0 +1,14 @@
+import { DBentry, DBtype } from "../../Shared/Isolat.model";
+import { generateCSVString } from "./generateCSVString.service";
+
+export function RAWDataStringGenerator(
+    rawKeys: DBtype[],
+    rawData: DBentry[]
+): string {
+    const RAWDataString: string[] = [];
+    const headers: DBtype[] = rawKeys;
+    RAWDataString.push(headers.join(","));
+    RAWDataString.push(generateCSVString(rawData, headers));
+
+    return RAWDataString.join("\n");
+}
