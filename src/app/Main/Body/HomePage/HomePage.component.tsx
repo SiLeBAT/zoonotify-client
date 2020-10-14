272b9a265415961e3bfae955780c03ad942b1f90
MicheleK17 <michele.kayser@bfr.bund.de>
2020-10-14 16:33:45 +0200

    Use Material-Ui Card for text components
        - home page and query page
    
    Ticket: #96
    Change-Id: Iac4d5422d54db6407cdc715b37ae827cd5463ed6

--- src/app/Main/Body/HomePage/HomePage.component.tsx
+++ src/app/Main/Body/HomePage/HomePage.component.tsx
@@ -1,60 +1,32 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { HomePageTextContentComponent as Content } from "./HomePage-TextContent.component";
-import {
-    surfaceColor,
-    shadowPalette,
-} from "../../../Shared/Style/Style-MainTheme.component";
+import { Card, CardMedia, makeStyles } from "@material-ui/core";
+import { HomePageTextContentComponent as TextContent } from "./HomePage-TextContent.component";
 
-const contentStyle = css`
-    height: 100%;
-    padding-top: 4rem;
-    padding-bottom: 5rem;
-    box-sizing: border-box;
-`;
-const boxStyle = css`
-    padding: 1em;
-    margin: auto;
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-self: flex-start;
-    flex: 0 1 auto;
-    hyphens: auto;
-    text-align: justify;
-    box-sizing: inherit;
-    border: 1px solid ${surfaceColor};
-    box-shadow: 0 3px 1px -2px ${shadowPalette.shadow1},
-        0 2px 2px 0 ${shadowPalette.shadow2},
-        0 1px 5px 0 ${shadowPalette.shadow3};
-`;
-const boxFooterStyle = css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-sizing: inherit;
-`;
 const logoStyle = css`
-    margin-top: 2rem;
-    max-width: 100%;
     height: auto;
-    margin-bottom: 2rem;
+    width: auto;
 `;
+const useStyles = makeStyles({
+    root: {
+        padding: "1em",
+        margin: "4rem auto",
+        width: "50%",
+    },
+});
 
 export function HomePageLayoutComponent(): JSX.Element {
+    const classes = useStyles();
     return (
-        <main css={contentStyle}>
-            <div css={boxStyle}>
-                <Content />
-                <div css={boxFooterStyle}>
-                    <img
-                        src="/assets/bfr_logo.gif"
-                        alt="BfR Logo"
-                        css={logoStyle}
-                    />
-                </div>
-            </div>
-        </main>
+        <Card className={classes.root}>
+            <TextContent />
+            <CardMedia
+                component="img"
+                alt="Contemplative Reptile"
+                image="/assets/bfr_logo.gif"
+                title="BfR Logo"
+                css={logoStyle}
+            />
+        </Card>
     );
 }
