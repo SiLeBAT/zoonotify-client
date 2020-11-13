b1fff9994943ab75f487c8dbad11808272ac1f51
MicheleK17 <michele.kayser@bfr.bund.de>
2020-11-13 18:44:26 +0100

    Implement page for external links
        - Implement navigation
        - Write JSON with link-information
        - Implement list with JSON input
        - Use translation
    
    Ticket: #50
    Change-Id: If9cd02b6c3e7869454db91a13f65a3006edc469a

---
+++ src/app/Main/Body/LinkPage/LinkPage.component.tsx
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import { Card, CardContent, makeStyles, Typography } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
+import { primaryColor } from "../../../Shared/Style/Style-MainTheme.component";
+import { LinkListComponent as LinkList} from "./LinkPage-List.component";
+
+const appNameStyle = css`
+    margin-bottom: 1rem;
+    font-size: 3rem;
+    text-align: center;
+    font-weight: normal;
+    color: ${primaryColor};
+`;
+const normalTextStyle = css`
+    font-size: 0.85rem;
+    line-height: 1.6;
+    text-align: justify;
+`;
+const useStyles = makeStyles({
+    root: {
+        padding: "1em",
+        margin: "4rem auto",
+        width: "50%",
+    },
+});
+
+export function LinkPageComponent(): JSX.Element {
+    const classes = useStyles();
+    const { t } = useTranslation(["ExternLinks"]);
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography variant="h1" css={appNameStyle}>
+                    {t("TextContent.Heading")}
+                </Typography>
+                <Typography css={normalTextStyle}>
+                    {t("TextContent.Text")}
+                </Typography>
+                {LinkList()}
+            </CardContent>
+        </Card>
+    );
+}
