3218d2be47d564f002ddff810cf5e167ed4b2d16
MicheleK17 <michele.kayser@bfr.bund.de>
2020-12-16 18:31:43 +0100

    Display Parameter in two columns
        - remove table
    
    Ticket: #169
    Change-Id: I60b37de8e15880b580f9aa5062fd8c94a83a1d96

--- src/app/Main/Body/QueryPage/Parameter/Parameter-List.component.tsx
+++ src/app/Main/Body/QueryPage/Parameter/Parameter-List.component.tsx
@@ -7,16 +7,23 @@ import {
     makeStyles,
     Theme,
     createStyles,
+    ListSubheader,
 } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
+import { FilterType } from "../../../../Shared/Filter.model";
 
-const tableStyle = css`
-    vertical-align: top;
-    padding-top: 18px;
-`;
-const parameterLabel = css`
+const spaceStyle = css`
+    padding: 0;
     margin: 0;
 `;
+const listStyle = css`
+    padding: 0;
+    margin: 0;
+    width: 50%;
+`;
 const parameterValue = css`
+    margin-top: 0;
+    margin-left: 2em;
     span {
         letter-spacing: 0;
     }
@@ -30,8 +37,11 @@ const useStyles = makeStyles((theme: Theme) =>
         demo: {
             backgroundColor: theme.palette.background.paper,
         },
-        title: {
-            margin: theme.spacing(4, 0, 2),
+        nested: {
+            paddingLeft: 0,
+            lineHeight: "20px",
+            color: "black",
+            fontWeight: "bold",
         },
     })
 );
@@ -41,7 +51,7 @@ function AddParameterToList(parameterList: string[]): JSX.Element[] {
     parameterList.forEach((element): void => {
         const keyName = element.replace(" ", "_");
         elements.push(
-            <ListItem key={`listItem-${keyName}`}>
+            <ListItem key={`listItem-${keyName}`} css={spaceStyle}>
                 <ListItemText
                     id={`labelId-${keyName}`}
                     primary={element}
@@ -54,28 +64,56 @@ function AddParameterToList(parameterList: string[]): JSX.Element[] {
 }
 
 interface ParameterListProps {
-    label: string;
-    keyName: string;
-    listElements: string[];
+    element1: FilterType;
+    element2: FilterType;
+    listElements1: string[];
+    listElements2: string[];
 }
 
 export function ParameterListComponent(props: ParameterListProps): JSX.Element {
     const classes = useStyles();
+    const { t } = useTranslation(["QueryPage"]);
 
-    return (
-        <tr key={`tr-${props.keyName}`}>
-            <td key={`td-heading-${props.keyName}`} css={tableStyle}>
-                <h4 css={parameterLabel}>{props.label}:</h4>
-            </td>
-            <td key={`tr-list-${props.keyName}`}>
+    const ListElement = (
+        element: FilterType,
+        attributeList: string[]
+    ): JSX.Element => {
+        if (element !== undefined) {
+            const keyName = element.replace(" ", "_");
+            const label = t(`Filters.${element}`);
+
+            return (
                 <List
                     dense
                     className={classes.root}
-                    key={`list-${props.keyName}`}
+                    key={`list-${keyName}`}
+                    css={listStyle}
+                    subheader={
+                        <ListSubheader
+                            component="div"
+                            id="nested-list-subheader"
+                            className={classes.nested}
+                        >
+                            {label}
+                        </ListSubheader>
+                    }
                 >
-                    {AddParameterToList(props.listElements)}
+                    {AddParameterToList(attributeList)}
                 </List>
-            </td>
-        </tr>
+            );
+        }
+        return <p css={listStyle}>&nbsp;</p>;
+    };
+
+    return (
+        <div
+            css={css`
+                display: flex;
+                flex-direction: row;
+            `}
+        >
+            {ListElement(props.element1, props.listElements1)}
+            {ListElement(props.element2, props.listElements2)}
+        </div>
     );
 }
