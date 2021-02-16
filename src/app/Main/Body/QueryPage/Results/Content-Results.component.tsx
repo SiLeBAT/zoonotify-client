be5615437061bdce6c1c51df7ac5d1df70b74fed
MicheleK17 <michele.kayser@bfr.bund.de>
2021-02-16 10:55:01 +0100

    Parse counted isolates form api to table format
    Ticket: #214
    Change-Id: Ic1235d0f97f2475db735c7a5a953cb879ad0a6ee

--- src/app/Main/Body/QueryPage/Results/Content-Results.component.tsx
+++ src/app/Main/Body/QueryPage/Results/Content-Results.component.tsx
@@ -1,126 +1,125 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import _ from "lodash";
 import { FilterContext } from "../../../../Shared/Context/FilterContext";
 import { FilterType } from "../../../../Shared/Model/Filter.model";
 import { TableContext } from "../../../../Shared/Context/TableContext";
 import { DataContext } from "../../../../Shared/Context/DataContext";
-import { DbCollection } from "../../../../Shared/Model/Client_Isolate.model";
-import { filterData } from "../../../../Core/FilterServices/filterData.service";
-import { getValuesOfOneFilterAttribute } from "../../../../Core/FilterServices/getValuesOfOneFilterAttribute.service";
-import { CheckIfFilterIsSet } from "../../../../Core/FilterServices/checkIfFilterIsSet.service";
 import { ResultsTableResultsComponent } from "./TableResults/Results-TableResults.component";
-import { generateRowsWithIsolates } from "./Results-RowsWithIsolates.service";
 import { calculateRowsWithPercent } from "./calculateRowsWithPercent.service";
+import { isolateCountURL } from "../../../../Shared/URLs";
+import { IsolateCountedDTO } from "../../../../Shared/Model/Api_Isolate.model";
 
 export function ContentResultsComponent(): JSX.Element {
-    const [state, setState] = useState<{
-        isCol: boolean;
-        isRow: boolean;
-    }>({
-        isCol: false,
-        isRow: false,
-    });
     const [columnAttributes, setColumnAttributes] = useState<string[]>([]);
     const { filter } = useContext(FilterContext);
     const { table, setTable } = useContext(TableContext);
-    const { data, setData } = useContext(DataContext);
-    const rowAttribute: FilterType = table.row;
-    const colAttribute: FilterType = table.column;
-
+    const { data } = useContext(DataContext);
+    const history = useHistory();
     const { t } = useTranslation(["QueryPage"]);
 
+    const rowAttribute: FilterType = table.row;
+    const colAttribute: FilterType = table.column;
+    const isCol: boolean = colAttribute !== "";
+    const isRow: boolean = rowAttribute !== "";
+    const state = { isCol, isRow };
+
     const getTableContext = async (
-        dataToCountIsolates: DbCollection
+        isolateCountProp: IsolateCountedDTO
     ): Promise<void> => {
-        const rowValues = getValuesOfOneFilterAttribute(
-            rowAttribute,
-            data.uniqueValues,
-            filter.selectedFilter
-        );
-        const colValues = getValuesOfOneFilterAttribute(
-            colAttribute,
-            data.uniqueValues,
-            filter.selectedFilter
-        );
-        const nrOfSelectedIsolates = dataToCountIsolates.length;
-        if (rowAttribute.length !== 0 && colAttribute.length !== 0) {
-            const rowsWithIsolates = generateRowsWithIsolates(
-                dataToCountIsolates,
-                colAttribute,
-                rowAttribute,
-                rowValues,
-                colValues,
-                "both"
-            );
-            const rowsWithPercent = calculateRowsWithPercent(rowsWithIsolates, nrOfSelectedIsolates)
-            setState({
-                isCol: true,
-                isRow: true,
+        if (!isCol && !isRow) {
+            setTable({
+                ...table,
+                statisticDataAbsolute: [],
             });
+        } else {
+            const rowValues: string[] = !isRow
+                ? [t("Results.TableHead")]
+                : (_.isEmpty(filter.selectedFilter[rowAttribute])
+                ? data.uniqueValues[rowAttribute]
+                : filter.selectedFilter[rowAttribute])
+
+            const colValues: string[] = !isCol
+                ? [t("Results.TableHead")]
+                : (_.isEmpty(filter.selectedFilter[colAttribute])
+                ? data.uniqueValues[colAttribute]
+                : filter.selectedFilter[colAttribute])
+
+            const isolateColValues: string[] = [t("Results.TableHead")];
+            const isolateRowValues: string[] = [t("Results.TableHead")];
+            isolateCountProp.groups.forEach((isolateCount) => {
+                isolateColValues.push(isolateCount[colAttribute]);
+                isolateRowValues.push(isolateCount[rowAttribute]);
+            });
+            const uniqIsolateColValues = _.uniq(isolateColValues);
+            const uniqIsolateRowValues = _.uniq(isolateRowValues);
+
+            const statisticTableDataAbs: Record<string, string>[] = [];
+
+            rowValues.forEach((rowElement) => {
+                const tempStatTable: Record<string, string> = {};
+                tempStatTable.name = rowElement;
+                colValues.forEach((colElement) => {
+                    if (
+                        _.includes(uniqIsolateColValues, colElement) &&
+                        _.includes(uniqIsolateRowValues, rowElement)
+                    ) {
+                        isolateCountProp.groups.forEach((isolateGroup) => {
+                            if (
+                                (isolateGroup[rowAttribute] === rowElement ||
+                                    isolateGroup[rowAttribute] === undefined) &&
+                                (isolateGroup[colAttribute] === colElement ||
+                                    isolateGroup[colAttribute] === undefined)
+                            ) {
+                                const statTableKey =
+                                    isolateGroup[colAttribute] === undefined
+                                        ? t("Results.TableHead")
+                                        : isolateGroup[colAttribute];
+                                tempStatTable[
+                                    statTableKey
+                                ] = (isolateGroup.count as unknown) as string;
+                            }
+                        });
+                    } else {
+                        tempStatTable[colElement] = "0";
+                    }
+                });
+                statisticTableDataAbs.push(tempStatTable);
+            });
+
+            const nrOfSelectedIsolates = isolateCountProp.totalNumberOfIsolates;
+            const rowsWithPercent = calculateRowsWithPercent(
+                statisticTableDataAbs,
+                nrOfSelectedIsolates
+            );
             setColumnAttributes(colValues);
             setTable({
                 ...table,
-                statisticDataAbsolute: rowsWithIsolates,
+                statisticDataAbsolute: statisticTableDataAbs,
                 statisticDataPercent: rowsWithPercent,
-            });
-        } else if (rowAttribute.length === 0 && colAttribute.length !== 0) {
-            const rowsWithIsolates = generateRowsWithIsolates(
-                dataToCountIsolates,
-                colAttribute,
-                rowAttribute,
-                [t("Results.TableHead")],
-                colValues,
-                "col"
-            );
-            const rowsWithPercent = calculateRowsWithPercent(rowsWithIsolates, nrOfSelectedIsolates)
-            setState({
-                isCol: true,
-                isRow: false,
-            });
-            setColumnAttributes(colValues);
-            setTable({
-                ...table,
-                statisticDataAbsolute: rowsWithIsolates,
-                statisticDataPercent: rowsWithPercent,
-            });
-        } else if (colAttribute.length === 0 && rowAttribute.length !== 0) {
-            const rowsWithIsolates = generateRowsWithIsolates(
-                dataToCountIsolates,
-                colAttribute,
-                rowAttribute,
-                rowValues,
-                [t("Results.TableHead")],
-                "row"
-            );
-            const rowsWithPercent = calculateRowsWithPercent(rowsWithIsolates, nrOfSelectedIsolates)
-            setState({
-                isCol: false,
-                isRow: true,
-            });
-            setColumnAttributes([t("Results.TableHead")]);
-            setTable({
-                ...table,
-                statisticDataAbsolute: rowsWithIsolates,
-                statisticDataPercent: rowsWithPercent,
-            });
-        } else if (rowAttribute.length === 0 && colAttribute.length === 0) {
-            setState({
-                isCol: false,
-                isRow: false,
             });
         }
     };
 
+    const ISOLATE_COUNT_URL: string = isolateCountURL + history.location.search;
+
+    const fetchIsolateCounted = async (): Promise<void> => {
+        const isolateCountResponse: Response = await fetch(ISOLATE_COUNT_URL);
+        const isolateCountProp: IsolateCountedDTO = await isolateCountResponse.json();
+
+        getTableContext(isolateCountProp);
+    };
+
     useEffect((): void => {
-        if (CheckIfFilterIsSet(filter.selectedFilter, filter.mainFilter)) {
-            setData({ ...data, ZNDataFiltered: data.ZNData });
-            getTableContext(data.ZNData);
-        } else {
-            const filteredData = filterData(data.ZNData, filter.selectedFilter, filter.mainFilter);
-            setData({ ...data, ZNDataFiltered: filteredData });
-            getTableContext(filteredData);
-        }
-    }, [filter, table.column, table.row, localStorage.getItem("i18nextLng")]);
+        fetchIsolateCounted();
+    }, [
+        filter,
+        table.column,
+        table.row,
+        ISOLATE_COUNT_URL,
+        localStorage.getItem("i18nextLng"),
+    ]);
 
     return (
         <ResultsTableResultsComponent
