4c3c4ea1258fce4b32e687af70f1bd7c38c6db66
MicheleK17 <michele.kayser@bfr.bund.de>
2021-08-04 13:16:33 +0200

    Load table only if ne feature is selected
        - little refactoring
    
    Ticket: #331
    Change-Id: Ice3775276c9536db06d68755b0bf03dafb8af99e

--- src/app/Main/Body/QueryPage/Drawer/Displayed_Features/DisplFeatures-Selector.component.tsx
+++ src/app/Main/Body/QueryPage/Drawer/Displayed_Features/DisplFeatures-Selector.component.tsx
@@ -6,7 +6,6 @@ import _ from "lodash";
 import { SelectorComponent } from "../../../../../Shared/Selector.component";
 import { FeatureType } from "../../../../../Shared/Context/DataContext";
 import { FilterType } from "../../../../../Shared/Model/Filter.model";
-import { generateFeatureList } from "./generateFeatureList.service";
 
 function generateTranslatedSelectorObject(
     selectorArray: string[],
@@ -58,29 +57,44 @@ export function DisplayedFeatureSelectorComponent(
         props.mainFilterAttributes,
         [props.otherFeature]
     );
-    const [isNotSelect, selectedValues]: [
-        boolean,
-        FilterType[]
-    ] = generateFeatureList(props.activeFeature);
+
+    const isNotSelect: boolean = _.isEmpty(props.activeFeature);
+    const selectedValue: FilterType | null = isNotSelect
+        ? null
+        : props.activeFeature;
 
     const dropDownValuesObj: {
         value: string;
         label: string;
     }[] = generateTranslatedSelectorObject(offeredAttributes, t);
-    const selectedValuesObj: {
-        value: string;
-        label: string;
-    }[] = generateTranslatedSelectorObject(selectedValues, t);
+
+    const selectedValueObj:
+        | {
+              value: string;
+              label: string;
+          }[]
+        | [] =
+        selectedValue === null
+            ? []
+            : [
+                  {
+                      value: selectedValue,
+                      label: t(`Filters.${selectedValue}`),
+                  },
+              ];
 
     const handleChange = (
         selectedOption: ValueType<{ value: string; label: string }>,
         keyName: FilterType | FeatureType
     ): void => {
         if (selectedOption !== undefined && !Array.isArray(selectedOption)) {
-            props.onChange(
-                selectedOption as { value: string; label: string } | null,
-                keyName
-            );
+            const option = selectedOption as { value: string; label: string };
+            if (option.value !== selectedValue) {
+                props.onChange(
+                    selectedOption as { value: string; label: string } | null,
+                    keyName
+                );
+            }
         } else {
             props.onChange(null, keyName);
         }
@@ -91,7 +105,7 @@ export function DisplayedFeatureSelectorComponent(
             label={props.label}
             noOptionLabel={t("Drawer.Selector")}
             dropDownValuesObj={dropDownValuesObj}
-            selectedValuesObj={selectedValuesObj}
+            selectedValuesObj={selectedValueObj}
             selectAttribute={props.selectAttribute}
             onChange={handleChange}
             isMulti={false}
