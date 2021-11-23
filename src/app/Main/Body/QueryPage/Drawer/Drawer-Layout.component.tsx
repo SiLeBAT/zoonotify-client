e011ce23b422214319c5af7310ce4ddab444f279
Dominic Tölle <dominic.tolle@gmail.com>
2021-11-23 16:05:10 +0100

    Fix Drawer-Layout
    Change-Id: I5d17aec2711348a85aa65e272d091fa58fb1ae29

--- src/app/Main/Body/QueryPage/Drawer/Drawer-Layout.component.tsx
+++ src/app/Main/Body/QueryPage/Drawer/Drawer-Layout.component.tsx
@@ -1,16 +1,47 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import Drawer from "@mui/material/Drawer";
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
 import { primaryColor } from "../../../../Shared/Style/Style-MainTheme.component";
 
 const filterHeadingStyle = css`
-margin-top: 0.5em;
-margin-bottom: 0.5em;
-font-weight: bold;
-font-size: 1.5rem;
-line-height: 2rem;
-color: ${primaryColor};
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  color: ${primaryColor};
 `;
+
+const ss = createStyles<
+  string,
+  {
+    drawerWidth: string;
+  }
+>({
+  drawer: ({ drawerWidth }) => ({
+    width: `${drawerWidth}px`,
+    minWidth: "325px",
+    position: "relative",
+    height: "100%",
+    zIndex: 0
+  }),
+  drawerPaper: {
+    width: "inherit",
+    zIndex: 0,
+    position: "relative",
+    direction: "rtl",
+    overflow: "auto"
+  },
+  drawerContainer: {
+    direction: "ltr",
+    padding: "1em",
+    height: "100%"
+  }
+});
+
+const useStyles = makeStyles(() => ss);
 
 /**
  * @desc Returns the Drawer
@@ -18,47 +49,32 @@ color: ${primaryColor};
  * @returns {JSX.Element} - Drawer component
  */
 export function DrawerLayoutComponent(props: {
-    filterSelection: JSX.Element;
-    displayFeatureSelection: JSX.Element;
-    isOpen: boolean;
-    drawerWidth: number;
-    drawerTitle: string;
+  filterSelection: JSX.Element;
+  displayFeatureSelection: JSX.Element;
+  isOpen: boolean;
+  drawerWidth: number;
+  drawerTitle: string;
 }): JSX.Element {
-    const drawerWidthSting = (props.drawerWidth as unknown) as string;
+  const drawerWidthSting = (props.drawerWidth as unknown) as string;
+  const classes = useStyles({
+    drawerWidth: drawerWidthSting
+  });
 
-
-    return (
-        <Drawer
-          sx={{
-            width: `${drawerWidthSting}px`,
-            minWidth: "325px",
-            position: "relative",
-            height: "100%",
-            zIndex: 0,
-            "& .MuiDrawer-paper": {
-              width: "inherit",
-              zIndex: 0,
-              position: "relative",
-              direction: "rtl",
-              overflow: "auto"
-            }
-          }}
-          variant="persistent"
-          anchor="left"
-          open={props.isOpen}
-        >
-          <div
-            style={{
-              direction: "ltr",
-              padding: "1em",
-              height: "100%"
-            }}
-          >
-            <p css={filterHeadingStyle}>{props.drawerTitle}</p>
-            {props.filterSelection}
-            {props.displayFeatureSelection}
-          </div>
-        </Drawer>
-      );
-
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={props.isOpen}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className={classes.drawerContainer}>
+        <p css={filterHeadingStyle}>{props.drawerTitle}</p>
+        {props.filterSelection}
+        {props.displayFeatureSelection}
+      </div>
+    </Drawer>
+  );
 }
