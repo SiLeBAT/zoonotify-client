6319d1041aeb659f710cddcaeb8c57739b5a2482
Michèle Kayser <64836751+MicheleK17@users.noreply.github.com>
2021-11-26 10:29:15 +0100

    Change drawer position to relative (#390)
    Ticket: #388
    Change-Id: If90433757336eef261c783a496c4fb96dd878227

--- src/app/Main/Body/QueryPage/Drawer/Drawer-Layout.component.tsx
+++ src/app/Main/Body/QueryPage/Drawer/Drawer-Layout.component.tsx
@@ -6,39 +6,39 @@ import makeStyles from "@mui/styles/makeStyles";
 import { primaryColor } from "../../../../Shared/Style/Style-MainTheme.component";
 
 const filterHeadingStyle = css`
-  margin-top: 0.5em;
-  margin-bottom: 0.5em;
-  font-weight: bold;
-  font-size: 1.5rem;
-  line-height: 2rem;
-  color: ${primaryColor};
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    color: ${primaryColor};
 `;
 
 const ss = createStyles<
-  string,
-  {
-    drawerWidth: string;
-  }
+    string,
+    {
+        drawerWidth: string;
+    }
 >({
-  drawer: ({ drawerWidth }) => ({
-    width: `${drawerWidth}px`,
-    minWidth: "325px",
-    position: "relative",
-    height: "100%",
-    zIndex: 0
-  }),
-  drawerPaper: {
-    width: "inherit",
-    zIndex: 0,
-    position: "relative",
-    direction: "rtl",
-    overflow: "auto"
-  },
-  drawerContainer: {
-    direction: "ltr",
-    padding: "1em",
-    height: "100%"
-  }
+    drawer: ({ drawerWidth }) => ({
+        width: `${drawerWidth}px`,
+        minWidth: "325px",
+        position: "relative",
+        height: "100%",
+        zIndex: 0,
+    }),
+    drawerPaper: {
+        width: "inherit",
+        zIndex: 0,
+        position: "relative",
+        direction: "rtl",
+        overflow: "auto",
+    },
+    drawerContainer: {
+        direction: "ltr",
+        padding: "1em",
+        height: "100%",
+    },
 });
 
 const useStyles = makeStyles(() => ss);
@@ -49,32 +49,38 @@ const useStyles = makeStyles(() => ss);
  * @returns {JSX.Element} - Drawer component
  */
 export function DrawerLayoutComponent(props: {
-  filterSelection: JSX.Element;
-  displayFeatureSelection: JSX.Element;
-  isOpen: boolean;
-  drawerWidth: number;
-  drawerTitle: string;
+    filterSelection: JSX.Element;
+    displayFeatureSelection: JSX.Element;
+    isOpen: boolean;
+    drawerWidth: number;
+    drawerTitle: string;
 }): JSX.Element {
-  const drawerWidthSting = (props.drawerWidth as unknown) as string;
-  const classes = useStyles({
-    drawerWidth: drawerWidthSting
-  });
+    const drawerWidthSting = (props.drawerWidth as unknown) as string;
+    const classes = useStyles({
+        drawerWidth: drawerWidthSting,
+    });
 
-  return (
-    <Drawer
-      className={classes.drawer}
-      variant="persistent"
-      anchor="left"
-      open={props.isOpen}
-      classes={{
-        paper: classes.drawerPaper
-      }}
-    >
-      <div className={classes.drawerContainer}>
-        <p css={filterHeadingStyle}>{props.drawerTitle}</p>
-        {props.filterSelection}
-        {props.displayFeatureSelection}
-      </div>
-    </Drawer>
-  );
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={props.isOpen}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+            sx={{ 
+                [`& .MuiDrawer-paper`]: {
+                    position: "relative",
+                    boxSizing: "border-box",
+                },
+            }}
+        >
+            <div className={classes.drawerContainer}>
+                <p css={filterHeadingStyle}>{props.drawerTitle}</p>
+                {props.filterSelection}
+                {props.displayFeatureSelection}
+            </div>
+        </Drawer>
+    );
 }
