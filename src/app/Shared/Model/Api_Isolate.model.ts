c5216562c450c942e1b21a2be39f7069ad6d447c
Michèle Kayser <64836751+MicheleK17@users.noreply.github.com>
2022-01-21 13:00:54 +0100

    Add proper export of AMRs for each isolate (#425)
    - distinguish between 1/0 and not tested (-)
    
    Ticket: #414

--- src/app/Shared/Model/Api_Isolate.model.ts
+++ src/app/Shared/Model/Api_Isolate.model.ts
@@ -1,3 +1,5 @@
+import { Resistances } from "./Client_Isolate.model";
+
 export interface IsolateDTO {
     isolates: {
         microorganism: string;
@@ -23,148 +25,13 @@ export interface IsolateDTO {
             e_hly: string;
             ampc_carba_phenotype: string;
         };
-        resistance: {
-            amp: {
+        resistance: Record<
+            Resistances,
+            {
                 value: string;
                 active: boolean;
-            };
-            azi: {
-                value: string;
-                active: boolean;
-            };
-            chl: {
-                value: string;
-                active: boolean;
-            };
-            cip: {
-                value: string;
-                active: boolean;
-            };
-            cli: {
-                value: string;
-                active: boolean;
-            };
-            col: {
-                value: string;
-                active: boolean;
-            };
-            dap: {
-                value: string;
-                active: boolean;
-            };
-            ery: {
-                value: string;
-                active: boolean;
-            };
-            etp: {
-                value: string;
-                active: boolean;
-            };
-            fep: {
-                value: string;
-                active: boolean;
-            };
-            fop: {
-                value: string;
-                active: boolean;
-            };
-            fox: {
-                value: string;
-                active: boolean;
-            };
-            fus: {
-                value: string;
-                active: boolean;
-            };
-            f_c: {
-                value: string;
-                active: boolean;
-            };
-            gen: {
-                value: string;
-                active: boolean;
-            };
-            imi: {
-                value: string;
-                active: boolean;
-            };
-            kan: {
-                value: string;
-                active: boolean;
-            };
-            lzd: {
-                value: string;
-                active: boolean;
-            };
-            mero: {
-                value: string;
-                active: boolean;
-            };
-            mup: {
-                value: string;
-                active: boolean;
-            };
-            nal: {
-                value: string;
-                active: boolean;
-            };
-            pen: {
-                value: string;
-                active: boolean;
-            };
-            rif: {
-                value: string;
-                active: boolean;
-            };
-            smx: {
-                value: string;
-                active: boolean;
-            };
-            str: {
-                value: string;
-                active: boolean;
-            };
-            syn: {
-                value: string;
-                active: boolean;
-            };
-            taz: {
-                value: string;
-                active: boolean;
-            };
-            tec: {
-                value: string;
-                active: boolean;
-            };
-            tet: {
-                value: string;
-                active: boolean;
-            };
-            tgc: {
-                value: string;
-                active: boolean;
-            };
-            tia: {
-                value: string;
-                active: boolean;
-            };
-            tmp: {
-                value: string;
-                active: boolean;
-            };
-            trm: {
-                value: string;
-                active: boolean;
-            };
-            t_c: {
-                value: string;
-                active: boolean;
-            };
-            van: {
-                value: string;
-                active: boolean;
-            };
-        };
+            }
+        >;
     }[];
 }
 
