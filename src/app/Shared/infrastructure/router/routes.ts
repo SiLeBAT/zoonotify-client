aa5aaf274b291ce1475f45b627dad98c1e68fd95
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-08-29 10:24:54 +0200

    Added environment specific config #554

--- src/app/Shared/infrastructure/router/routes.ts
+++ src/app/Shared/infrastructure/router/routes.ts
@@ -6,7 +6,7 @@ export const pageRoute = {
     dpdPagePath: "/dataProtectionDeclaration",
 };
 
-export const CMS_BASE_ENDPOINT = `http://localhost:1337`;
+export const CMS_BASE_ENDPOINT = process.env.REACT_APP_API_URL;
 export const CMS_API_ENDPOINT = CMS_BASE_ENDPOINT + `/api`;
 export const CONFIGURATION = `${CMS_API_ENDPOINT}/configuration`;
 export const WELCOME = `${CMS_API_ENDPOINT}/welcome`;
