118557e9e75cc704f9704aca3876db0ac11a9010
mahtabiltarabian <mahtab@host43.lan>
2023-10-09 14:35:47 +0200

    #560-replace hard coded externallinks with cms privider link

--- src/app/pages/links/LinkList-ListContent.component.tsx
+++ src/app/pages/links/LinkList-ListContent.component.tsx
@@ -2,26 +2,21 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { ListContentListItemComponent } from "./ListContent-ListItem.component";
 
-/**
- * @desc Returns ListItems for each link
- * @param category - category of the links to find it in the translation file
- * @param linkList - list of links belonging to one category
- * @returns {JSX.Element[]} - list of ListItems for each link
- */
-export function LinkListContentComponent(
-    category: string,
-    linkList: Record<string, string>[]
-): JSX.Element[] {
+interface PropsType {
+    category: string;
+    linkList: Record<string, string>[];
+}
+
+export function LinkListContentComponent({
+    category,
+    linkList,
+}: PropsType): JSX.Element[] {
     const { t } = useTranslation(["ExternLinks"]);
-    const elements: JSX.Element[] = [];
-    linkList.map((linkObject, index) =>
-        elements.push(
-            <ListContentListItemComponent
-                key={`ListContentListItemComponent` + index}
-                link={linkObject.Link}
-                text={t(`${category}.${linkObject.Text}`)}
-            />
-        )
-    );
-    return elements;
+    return linkList.map((linkObject, index) => (
+        <ListContentListItemComponent
+            key={`ListContentListItemComponent` + index}
+            link={linkObject.Link}
+            text={t(`${category}.${linkObject.Name}`)}
+        />
+    ));
 }
