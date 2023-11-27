4d4f3161b04cb78b9c938a4adccb7ab99293906a
mahtabiltarabian <m.iltarabian68@gmail.com>
2023-11-27 15:01:20 +0100

    699 change client version

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = (env, argv) => {
                 path: `./.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`
             }),
             new webpack.DefinePlugin({
-                webappVersion: env.version ? JSON.stringify(env.version) : JSON.stringify('0.0.1'),
+                webappVersion: env.version ? JSON.stringify(env.version) : JSON.stringify('1.0.0'),
                 lastChange: env.lastChange ? JSON.stringify(env.lastChange): JSON.stringify(new Date().toISOString()),
             })
         ]
