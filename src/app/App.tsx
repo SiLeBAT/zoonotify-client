c0a53cd065b2924487295643d1d471a763009503
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-02-15 22:18:33 +0100

    As a smartphone user I want to be informed that the Zoonotify Webapp has not been optimized for mobile screens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
-import React, { Suspense } from "react";
+import React, { ReactElement, Suspense, useEffect } from "react";
 import * as ReactDOM from "react-dom";
 import "../i18n";
 import { LoadingProcessComponent } from "./shared/components/loading_process/LoadingProcess.component";
@@ -8,11 +8,29 @@ import { znTheme } from "./shared/style/Style-MainTheme";
 
 const theme = znTheme;
 
+function detectMobileDevice(): boolean {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /iphone|ipod|android|ie|blackberry|fennec/.test(userAgent);
+    return isMobile;
+}
+
+function App(): ReactElement {
+    useEffect(() => {
+        if (detectMobileDevice()) {
+            alert(
+                "Please note: The Zoonotify Webapp has not been optimized for mobile screens."
+            );
+        }
+    }, []);
+
+    return <MainLayoutComponent />;
+}
+
 ReactDOM.render(
     <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
             <Suspense fallback={<LoadingProcessComponent />}>
-                <MainLayoutComponent />
+                <App />
             </Suspense>
         </ThemeProvider>
     </StyledEngineProvider>,
