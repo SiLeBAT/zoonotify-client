bf52475563bd911e73edaf6e4e60d7ae69a4f15e
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-03-18 17:09:54 +0100

    extract http://localhost:8081 into a global constant

--- cypress/e2e/1-getting-started/Page Availability Tests.cy.js
+++ cypress/e2e/1-getting-started/Page Availability Tests.cy.js
@@ -1,38 +1,32 @@
 describe('Page Availability Tests', () => {
   it('Checks if the Explanations page is available', () => {
-    cy.visit('http://localhost:8081/explanations');
+    cy.visit(Cypress.env('BASE_URL') + '/explanations');
     cy.url().should('include', '/explanations');
     cy.get('body').should('contain', 'Erläuterungen');
- 
-    
   });
 
   it('Checks if the Evaluations page is available', () => {
-    cy.visit('http://localhost:8081/evaluations');
+    cy.visit(Cypress.env('BASE_URL') + '/evaluations');
     cy.url().should('include', '/evaluations');
     cy.get('body').should('contain', 'Erläuterungen');
-   
-    
   });
 
   it('Checks if the Prevalence page is available', () => {
-    cy.visit('http://localhost:8081/prevalence');
+    cy.visit(Cypress.env('BASE_URL') + '/prevalence');
     cy.url().should('include', '/prevalence');
     cy.get('body').should('contain', 'Prävalenz');
-    
   });
 
   it('Checks if the External Links page is available', () => {
-    cy.visit('http://localhost:8081/externallinks');
+    cy.visit(Cypress.env('BASE_URL') + '/externallinks');
     cy.url().should('include', '/externallinks');
     cy.get('body').should('contain', 'Externe Links');
-    
   });
 
   it('Checks if the LD page is available', () => {
-    cy.visit('http://localhost:8081/ld');
+    cy.visit(Cypress.env('BASE_URL') + '/ld');
     cy.url().should('include', '/ld');
-    cy.get('body').should('contain', 'Linked Data Playground');
-    
-  });
+    cy.get('body').
+should('contain', 'Linked Data Playground');
+});
 });
