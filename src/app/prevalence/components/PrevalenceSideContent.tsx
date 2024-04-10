06078d9010e1c25fb245e0f4cbabd301f2d5a0ac
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-04-10 14:11:40 +0200

    Reuse the filter multy selection componnet in prevalence page

--- src/app/prevalence/components/PrevalenceSideContent.tsx
+++ src/app/prevalence/components/PrevalenceSideContent.tsx
@@ -1,28 +1,19 @@
 import React from "react";
+import FilterListIcon from "@mui/icons-material/FilterList";
 import {
     Box,
     Button,
-    Checkbox,
-    FormControl,
-    FormControlLabel,
-    FormGroup,
-    FormLabel,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
-    Typography,
     styled,
+    Accordion,
+    AccordionDetails,
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import { useTranslation } from "react-i18next";
 import { usePrevalenceSideContent } from "./usePrevalenceSideContent";
+import { useTranslation } from "react-i18next";
+import { FilterMultiSelectionComponent } from "../../evaluations/components/FilterMultiSelectionComponent";
 
 const StyledAccordion = styled(Accordion)(({ theme }) => ({
-    marginBottom: theme.spacing(4),
+    marginBottom: theme.spacing(1),
     backgroundColor: "white",
-    borderBottom: `1px solid ${theme.palette.divider}`,
-    borderRight: `1px solid ${theme.palette.divider}`,
     boxShadow: "none",
     "&:before": {
         display: "none",
@@ -33,123 +24,80 @@ const StyledAccordion = styled(Accordion)(({ theme }) => ({
 }));
 
 export function PrevalenceSideContent(): JSX.Element {
+    const { t } = useTranslation(["PrevalencePage"]);
     const {
         selectedMicroorganisms,
-
         selectedAnimalSpecies,
-        selectAllMicroorganisms,
-        selectAllAnimalSpecies,
-        handleMicroorganismChange,
-        handleAnimalSpeciesChange,
+        handleMicroorganismSelectionChange,
+        handleAnimalSpeciesSelectionChange,
         microorganismOptions,
         animalSpeciesOptions,
     } = usePrevalenceSideContent();
 
-    const { t } = useTranslation(["PrevalencePage"]);
+    const microorganismSelectionOptions = microorganismOptions.map(
+        (option) => ({ value: option, displayName: option })
+    );
+    const animalSpeciesSelectionOptions = animalSpeciesOptions.map(
+        (option) => ({ value: option, displayName: option })
+    );
+
+    const handleMicroorganismsChange = (selected: string[]): void => {
+        handleMicroorganismSelectionChange(selected);
+    };
+
+    const handleAnimalSpeciesChange = (selected: string[]): void => {
+        handleAnimalSpeciesSelectionChange(selected);
+    };
 
     return (
-        <Box sx={{ padding: 2, height: "100vh", overflowY: "auto" }}>
+        <Box
+            sx={{
+                padding: 0,
+                height: "100vh",
+                overflowY: "auto",
+                width: "100%",
+                minWidth: "300px",
+            }}
+        >
             <StyledAccordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography>{t("microorganism")}</Typography>
-                </AccordionSummary>
                 <AccordionDetails>
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend">
-                            <Button
-                                onClick={() => selectAllMicroorganisms(true)}
-                            >
-                                {t("SELECT_ALL")}
-                            </Button>
-                            <Button
-                                onClick={() => selectAllMicroorganisms(false)}
-                            >
-                                {t("DESELECT_ALL")}
-                            </Button>
-                        </FormLabel>
-                        <FormGroup>
-                            {microorganismOptions.map((option) => (
-                                <FormControlLabel
-                                    key={option}
-                                    control={
-                                        <Checkbox
-                                            checked={selectedMicroorganisms.includes(
-                                                option
-                                            )}
-                                            onChange={(event) =>
-                                                handleMicroorganismChange(
-                                                    event,
-                                                    option
-                                                )
-                                            }
-                                            name={option}
-                                        />
-                                    }
-                                    label={option}
-                                />
-                            ))}
-                        </FormGroup>
-                    </FormControl>
+                    <FilterMultiSelectionComponent
+                        selectedItems={selectedMicroorganisms}
+                        selectionOptions={microorganismSelectionOptions}
+                        name="microorganisms"
+                        label={t("Microorganism")}
+                        actions={{
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            handleChange: (event: any) =>
+                                handleMicroorganismsChange(event.target.value),
+                        }}
+                    />
                 </AccordionDetails>
             </StyledAccordion>
 
             <StyledAccordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Typography>{t("animalSpecies")}</Typography>
-                </AccordionSummary>
                 <AccordionDetails>
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend">
-                            <Button
-                                onClick={() => selectAllAnimalSpecies(true)}
-                            >
-                                {t("SELECT_ALL")}
-                            </Button>
-                            <Button
-                                onClick={() => selectAllAnimalSpecies(false)}
-                            >
-                                {t("DESELECT_ALL")}
-                            </Button>
-                        </FormLabel>
-                        <FormGroup>
-                            {animalSpeciesOptions.map((option) => (
-                                <FormControlLabel
-                                    key={option}
-                                    control={
-                                        <Checkbox
-                                            checked={selectedAnimalSpecies.includes(
-                                                option
-                                            )}
-                                            onChange={(event) =>
-                                                handleAnimalSpeciesChange(
-                                                    event,
-                                                    option
-                                                )
-                                            }
-                                            name={option}
-                                        />
-                                    }
-                                    label={option}
-                                />
-                            ))}
-                        </FormGroup>
-                    </FormControl>
+                    <FilterMultiSelectionComponent
+                        selectedItems={selectedAnimalSpecies}
+                        selectionOptions={animalSpeciesSelectionOptions}
+                        name="animalSpecies"
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        label={t("Animal Species")}
+                        actions={{
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            handleChange: (event: any) =>
+                                handleAnimalSpeciesChange(event.target.value),
+                        }}
+                    />
                 </AccordionDetails>
             </StyledAccordion>
 
+            {/* Filter Button */}
             <Box
                 sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}
             >
                 <Button variant="contained" startIcon={<FilterListIcon />}>
-                    {t("FILTER")}
+                    {t("Filter")}
                 </Button>
             </Box>
         </Box>
