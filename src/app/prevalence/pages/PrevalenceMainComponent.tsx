880903ba5f6a29938a46a393c412c410436f524a
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-04-25 12:23:24 +0200

    Add Prevalence Data Table
    Ticket: #827

--- src/app/prevalence/pages/PrevalenceMainComponent.tsx
+++ src/app/prevalence/pages/PrevalenceMainComponent.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { MainWithSideLayout } from "../../shared/components/layout/MainWithSideLayoutComponent";
+import { PageLayoutComponent } from "../../shared/components/layout/PageLayoutComponent";
 import { PrevalenceDataProvider } from "../components/PrevalenceDataContext"; // Import the provider
-import PrevalenceMainContent  from "../components/PrevalenceMainContent";
+import { PrevalenceMainContent } from "../components/PrevalenceMainContent";
 import { PrevalenceSideContent } from "../components/PrevalenceSideContent";
 import { usePrevalencePageComponent } from "./prevalenceUseCases";
-import { PageLayoutComponent } from "../../shared/components/layout/PageLayoutComponent";
 
 export function PrevalenceMainComponent(): React.ReactElement {
     const { model } = usePrevalencePageComponent();
